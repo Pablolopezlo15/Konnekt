86ae9bf306ba2cac1a94eb2c8ebe008624186192
Pablolopezlo15 <plopezlozano12@gmail.com>
2025-05-07 20:04:48 +0200

    update

---
+++ Backend/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /app/certs
+
+if [ ! -f /app/certs/key.pem ]; then
+  echo "🔑 Generando claves SSL..."
+  openssl genrsa -out /app/certs/key.pem 2048
+  openssl req -new -x509 -key /app/certs/key.pem -out /app/certs/cert.pem -days 365 -subj "/CN=localhost"
+fi
+
+exec uvicorn main2:app --host 0.0.0.0 --port 443 \
+  --ssl-keyfile /app/certs/key.pem \
+  --ssl-certfile /app/certs/cert.pem
